2cd33f67da5e7544ad2538e9207e3b7be15f631e
theknzz <joaquimpersonalmail@isec.pt>
2020-10-20 16:05:06 +0100

    Updates

---
+++ precache-manifest.63bec0cea44144a7f3113ab2bcd9fced.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac3854daf2436cf33aee3b5388dbb213",
+    "url": "/KanbanBoard/index.html"
+  },
+  {
+    "revision": "1274122507277d0b59d7",
+    "url": "/KanbanBoard/static/css/main.9d180a72.chunk.css"
+  },
+  {
+    "revision": "ad7b68e8544db5505667",
+    "url": "/KanbanBoard/static/js/2.b2dbd580.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/KanbanBoard/static/js/2.b2dbd580.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1274122507277d0b59d7",
+    "url": "/KanbanBoard/static/js/main.9575c18c.chunk.js"
+  },
+  {
+    "revision": "ec5342e4bf8964c21276",
+    "url": "/KanbanBoard/static/js/runtime-main.9c75f76a.js"
+  }
+]);
